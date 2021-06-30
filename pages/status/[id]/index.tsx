6db877ea60cd44610061ad51099f21a94d434950
josejavier <82359964+josejaviersanahuja@users.noreply.github.com>
2021-06-30 12:01:35 +0100

    hemos añadido firebaseAdmin para permitir el fetching de datos desde cualquier cliente final
    hemos tenido que crear archivos .env y un json con claves privadas. Sin eso, no podemos lanzarnos a produccion

---
+++ pages/status/[id]/index.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-use-before-define */
+import React, { ReactElement } from 'react'
+
+export default function index (): ReactElement {
+  return (
+        <div>
+            esta es una página que debería mostrar un twit
+        </div>
+  )
+}
